ea0b3bbc965f874e1bca5e06cda57c4b0b21e1e9
Joy10311 <joy10311@gmail.com>
2021-09-02 15:11:49 +0600

    fourth commit

--- js/index.js
+++ js/index.js
@@ -1,19 +1,44 @@
+document.getElementById('error-message').style.display = 'none';
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
     // clear data 
+    // searchResult.textContent = '';
     searchField.value = '';
-    const url = ` http://openlibrary.org/search.json?q=${searchText}`
+    document.getElementById('error-message').style.display = 'none';
+    if (searchText === '') {
+        // please write something to display
+        displayError();
+        console.log('error')
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displySearchResult(data.docs))
+    }
+    else {
+        // load data 
+        const url = ` https://openlibrary.org/search.json?q=${searchText}`
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displySearchResult(data.docs))
+            .catch(error => displayError(error));
+    }
+
 }
+const displayError = error => {
+    document.getElementById('error-message').style.display = 'block';
+}
+
 
 const displySearchResult = docs => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
+
+    if (docs.length === 0) {
+        // show no result found;
+        console.log('error3')
+        displayError()
+    }
+
     docs.forEach(doc => {
         console.log(doc)
         const div = document.createElement('div');
@@ -28,6 +53,9 @@ const displySearchResult = docs => {
         </div>
       </div>
         `;
+
         searchResult.appendChild(div);
+
+
     })
 }
