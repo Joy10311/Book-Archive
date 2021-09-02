e28efbafe063a804d1196cf660f3ca32ce3f6e9d
Joy10311 <joy10311@gmail.com>
2021-09-02 11:34:58 +0600

    third commit

--- js/index.js
+++ js/index.js
@@ -19,15 +19,12 @@ const displySearchResult = docs => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="col">
         <div class="card">
-          <img src=" https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top " alt="...">
-          <div class="card-body">
-        <h5 class="card-title">${doc.title}</h5>
-        <p class="card-text">Author: ${doc.author_name}</p>
-        <p class="card-text">First publish date: ${doc.first_publish_year}</p>
-        <p class="card-text">.</p>
-          </div>
+        <img src=" https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top " alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${doc.title}</h5>
+          <p class="card-text">Author: ${doc.author_name}</p>
+          <p class="card-text">First publish date: ${doc.first_publish_year}</p>
         </div>
       </div>
         `;
